bbea17d8b864b67e0191d4cbe284dd66f2c22ef5
Elias Ouazzani <elias.oc.2007@gmail.com>
2026-05-18 08:12:57 -0400

    Add clear all button

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from nicegui import ui
 
 def add_task():
-      ui.label(task_input.value)
+      with task_list:
+          ui.label(task_input.value)
       task_input.value = ""
+
+def clear_tasks():
+      task_list.clear()
 
 if __name__ in {"__main__", "__mp_main__"}:
       ui.label("My to-do list")
       task_input = ui.input("Enter task")
       ui.button("Add task!", on_click=add_task)
+      ui.button("Clear all", on_click=clear_tasks)
+      task_list = ui.column()
       ui.run()
